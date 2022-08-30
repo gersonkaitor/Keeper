0f04463897935072180daadff6b0058931b689dd
gersonkaitor <gersonkaitor@gmail.com>
2022-08-30 17:47:02 +0800

    Keeper Part 1

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note(){
+    return(
+        <div className="note">
+            <h1>Note Title</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    );
+}
