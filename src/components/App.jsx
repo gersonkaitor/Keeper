49c879fe6d855b14e05af231b5cd678a41c0e77c
gersonkaitor <gersonkaitor@gmail.com>
2022-09-13 21:16:25 +0800

    Keeper Part 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes"
+//import notes from "../notes"
+import CreateArea from "./CreateArea";
 
 
 
 export default function App(){
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes((prevNotes) =>{
+            return [...prevNotes, newNote]
+        })        
+    }
+
+    function deleteNote(id){
+       setNotes((prevNotes) =>{
+         return prevNotes.filter((note, index) =>{
+            return index !== id;
+         })
+       })
+    }
+
+    
+
     return(
         <div>
             <Header />
+            <CreateArea onAdd={addNote}/>
+            {/* <Note key={1} title="Note title" content="Note content" /> */}
             {
-                notes.map(noteItem => <Note 
-                        key={noteItem.key}
+                notes.map((noteItem,index) => <Note 
+                        key={index}
+                        id={index}
                         title={noteItem.title}
                         content={noteItem.content}
+                        onDelete={deleteNote}
                     />
                 )             
             }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default function Note(prop){
+export default function Note(props){
+
+    function handleClick(){
+        props.onDelete(props.id)
+    }
+
     return(
 
                 <div className="note">
-                    <h1>{prop.title}</h1>
-                    <p>{prop.content}</p>
+                    <h1>{props.title}</h1>
+                    <p>{props.content}</p>
+                    <button onClick={handleClick}>DELETE</button>
                 </div>
                 
             )
